2022458d721e97d93468aa5245f57f6e14854c0f
Khanjan <17khanjan@gmail.com>
2022-10-19 15:27:10 +0530

    1st commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/RainBite/';
+$config['base_url'] = 'https://rainbite.in/';
 
 /*
 |--------------------------------------------------------------------------
